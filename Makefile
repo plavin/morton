576b020c06ff134162b9c2fb15c8b9a462e87d19
Patrick Lavin <plavin3@gatech.edu>
2020-01-13 22:25:52 -0500

    backup

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -o morton main.c -O3
---
+++ temp/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+    printf("sizeof(unsigned): %zu\n", sizeof(unsigned));
+    printf("sizeof(unsigned long): %zu\n", sizeof(unsigned long));
+
+}
