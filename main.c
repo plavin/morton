1ae4ebef0a6c71bb875b7a18fadf916aa32a88e4
Patrick Lavin <plavin3@gatech.edu>
2020-01-13 22:30:28 -0500

    clean up

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 #include <stdint.h>
 #include <assert.h>
 
-
-unsigned morton_1(unsigned long x)
+uint32_t morton_1(uint64_t x)
 {
     x = x & 0x5555555555555555;
     x = (x | (x >> 1))  & 0x3333333333333333;
@@ -12,63 +11,56 @@ unsigned morton_1(unsigned long x)
     x = (x | (x >> 4))  & 0x00FF00FF00FF00FF;
     x = (x | (x >> 8))  & 0x0000FFFF0000FFFF;
     x = (x | (x >> 16)) & 0x00000000FFFFFFFF;
-    return (unsigned)x;
+    return (uint32_t)x;
 }
 
-void d2xy_morton(unsigned long d, unsigned *x, unsigned *y)
+void d2xy_morton(uint64_t d, uint32_t *x, uint32_t *y)
 {
     *x = morton_1(d);
     *y = morton_1(d >> 1);
 }
 
-void z_ordering_2d_2(unsigned dim, unsigned npow2)
+void z_ordering_2d_2(uint32_t dim, uint32_t npow2)
 {
-    unsigned ii, jj;
-    unsigned extra = 0;
-    for (unsigned i = 0; i < dim * dim + extra; i++) {
+    uint32_t ii, jj;
+    uint32_t extra = 0;
+    for (uint32_t i = 0; i < dim * dim + extra; i++) {
         d2xy_morton(i, &jj, &ii);
         if (ii >= dim || jj >= dim) {
             extra++;
             continue;
         }
-        printf("(%u,%u) -> %lu\n", ii, jj, ((unsigned long)ii)*dim + jj);
+        printf("(%u,%u) -> %lu\n", ii, jj, ((uint64_t)ii)*dim + jj);
     }
 }
 
-unsigned long *z_order_list(unsigned dim)
+uint64_t *z_order_list(uint32_t dim)
 {
-    unsigned npow2 = 0;
-    unsigned dim_copy = dim;
+    uint32_t npow2 = 0;
+    uint32_t dim_copy = dim;
     while (dim_copy >>= 1) npow2++;
     if (1<<npow2 != dim) npow2++;
-    unsigned long *list = (unsigned long*) malloc (sizeof(unsigned long) * dim * dim);
+    uint64_t *list = (uint64_t*) malloc (sizeof(uint64_t) * dim * dim);
     assert(list);
 
-    unsigned ii, jj;
-    unsigned extra = 0;
-    unsigned long idx = 0;
-    for (unsigned long i = 0; i < dim * dim + extra; i++) {
+    uint32_t ii, jj;
+    uint32_t extra = 0;
+    uint64_t idx = 0;
+    for (uint64_t i = 0; i < dim * dim + extra; i++) {
         d2xy_morton(i, &jj, &ii);
         if (ii >= dim || jj >= dim) {
             extra++;
             continue;
         }
-        list[idx++] = ((unsigned long)ii)*dim + jj;
+        list[idx++] = ((uint64_t)ii)*dim + jj;
     }
      return list;
 }
 
 int main (int argc, char **argv)
 {
-    unsigned long dim_l;
-    unsigned dim, dim_copy, npow2;
-
-    /*
-    unsigned x1 = 0;
-    unsigned x2 = 2;
-    printf("(%u,%u)->%lu\n", x1, x2, mix_2d(x2, x1));
-    exit(0);
-    */
+    uint64_t dim_l;
+    uint32_t dim, dim_copy, npow2;
 
     if (argc != 2) {
         printf("Usage: %s <dim>\n", argv[0]);
@@ -76,7 +68,7 @@ int main (int argc, char **argv)
     }
 
     dim_l = strtoul (argv[1], NULL, 0);
-    dim = (unsigned) dim_l;
+    dim = (uint32_t) dim_l;
 
     if (dim_l > dim) {
         printf("Error: Dimension is too big to be represented by 32 bits\n");
@@ -96,11 +88,13 @@ int main (int argc, char **argv)
     printf("The number of bits needed to store dimension is: %u\n", npow2);
 
     z_ordering_2d_2(dim, npow2);
-    unsigned long *list = z_order_list(dim);
+    uint64_t *list = z_order_list(dim);
 
     for (int i = 0; i < dim*dim; i++) {
         printf("%lu ", list[i]);
     }
     printf("\n");
+
+    free(list);
 
 }
