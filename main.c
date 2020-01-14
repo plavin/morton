c992e4edd1b1e3c271d4ab093a0381c069d91a5c
Patrick Lavin <plavin3@gatech.edu>
2020-01-14 15:18:09 -0500

    cleanup, begin 3d work

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
+// Author: Patrick Lavin
+// This code prints a 2d morton ordering
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
 
-uint32_t morton_1(uint64_t x)
+// Taken from https://stackoverflow.com/a/30562230
+uint32_t even_bits(uint64_t x)
 {
     x = x & 0x5555555555555555;
     x = (x | (x >> 1))  & 0x3333333333333333;
@@ -14,54 +17,66 @@ uint32_t morton_1(uint64_t x)
     return (uint32_t)x;
 }
 
+// Taken from https://stackoverflow.com/a/30562230
 void d2xy_morton(uint64_t d, uint32_t *x, uint32_t *y)
 {
-    *x = morton_1(d);
-    *y = morton_1(d >> 1);
+    *x = even_bits(d);
+    *y = even_bits(d >> 1);
 }
 
-void z_ordering_2d_2(uint32_t dim, uint32_t npow2)
+// TODO: Make everything 64 bits, manually check that dim fits
+uint64_t morton3(uint64_t x) {
+    x = x & 0x9249249249249249;
+    x = (x | (x >> 2))  & 0x30c30c30c30c30c3;
+    x = (x | (x >> 4))  & 0xf00f00f00f00f00f;
+    x = (x | (x >> 8))  & 0x00ff0000ff0000ff;
+    x = (x | (x >> 16)) & 0xffff00000000ffff;
+    x = (x | (x >> 32)) & 0x00000000ffffffff;
+    return x;
+}
+/*
+uint64_t bits;
+uint64_t x = morton3(bits)
+uint64_t y = morton3(bits>>1)
+uint64_t z = morton3(bits>>2)
+*/
+
+void z_ordering_2d(uint32_t dim)
 {
-    uint32_t ii, jj;
-    uint32_t extra = 0;
-    for (uint32_t i = 0; i < dim * dim + extra; i++) {
-        d2xy_morton(i, &jj, &ii);
-        if (ii >= dim || jj >= dim) {
+    uint32_t x, y, extra = 0;
+    uint64_t i;
+
+    for (i = 0; i < dim * dim + extra; i++) {
+        d2xy_morton(i, &y, &x);
+        if (x >= dim || y >= dim) {
             extra++;
             continue;
         }
-        printf("(%u,%u) -> %lu\n", ii, jj, ((uint64_t)ii)*dim + jj);
+        printf("(%u,%u) -> %lu\n", x, y, ((uint64_t)x)*dim + y);
     }
 }
 
-uint64_t *z_order_list(uint32_t dim)
+void z_order_list(uint32_t dim, uint64_t *list)
 {
-    uint32_t npow2 = 0;
-    uint32_t dim_copy = dim;
-    while (dim_copy >>= 1) npow2++;
-    if (1<<npow2 != dim) npow2++;
-    uint64_t *list = (uint64_t*) malloc (sizeof(uint64_t) * dim * dim);
-    assert(list);
+    uint32_t x, y, idx = 0, extra = 0;
+    uint64_t i;
 
-    uint32_t ii, jj;
-    uint32_t extra = 0;
-    uint64_t idx = 0;
-    for (uint64_t i = 0; i < dim * dim + extra; i++) {
-        d2xy_morton(i, &jj, &ii);
-        if (ii >= dim || jj >= dim) {
+    for (i = 0; i < dim * dim + extra; i++) {
+        d2xy_morton(i, &y, &x);
+        if (x >= dim || y >= dim) {
             extra++;
             continue;
         }
-        list[idx++] = ((uint64_t)ii)*dim + jj;
+        list[idx++] = ((uint64_t)x)*dim + y;
     }
-     return list;
 }
 
 int main (int argc, char **argv)
 {
     uint64_t dim_l;
-    uint32_t dim, dim_copy, npow2;
+    uint32_t dim;
 
+    // Check args
     if (argc != 2) {
         printf("Usage: %s <dim>\n", argv[0]);
         exit(1);
@@ -79,17 +94,16 @@ int main (int argc, char **argv)
         printf("Error: Dimension must be positive\n");
     }
 
-    dim_copy = dim;
-    npow2 = 0;
-    while (dim_copy >>= 1) npow2++;
-    if (1<<npow2 != dim) npow2++;
-
+    // Print ordering as x, y pairs
     printf("The grid size is: (%u,%u)\n", dim, dim);
-    printf("The number of bits needed to store dimension is: %u\n", npow2);
+    z_ordering_2d(dim);
 
-    z_ordering_2d_2(dim, npow2);
-    uint64_t *list = z_order_list(dim);
+    // Get ordering as indices into 1d array of size dim x dim
+    uint64_t *list = (uint64_t*) malloc (sizeof(uint64_t) * dim * dim);
+    assert(list);
+    z_order_list(dim, list);
 
+    // Print list
     for (int i = 0; i < dim*dim; i++) {
         printf("%lu ", list[i]);
     }
